8c66a5e09e1016949680307ab3d084161cf3dcea
DESKTOP-KHVMTIT <rohit828396@gmail.com>
2022-09-12 13:06:44 +0530

    changes

--- src/test/java/Testcases/launch.java
+++ src/test/java/Testcases/launch.java
@@ -1,7 +1,14 @@
 package Testcases;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -26,7 +33,24 @@ public class launch {
 	softAssert.assertEquals(currenturl, "https:www.coforge.com");
 	System.out.println("ok");
 	driver.quit();
-	//softAssert.assertAll();
+	softAssert.assertAll();
+	
+	
+	
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(120));
+	
+	WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(120));
+	wait.until(ExpectedConditions.elementToBeClickable(By.id("Coforge")));
+	
+	
+	
+	FluentWait wait = new FluentWait(driver);
+	wait.withTimeout(Duration.ofSeconds(120));
+	wait.pollingEvery(Duration.ofSeconds(5));
+	//wait.until(ExpectedConditions.elementToBeClickable(null));
+	
+	
+	
 	
 	
 	
---
+++ src/test/java/Testcases/push.java
@@ -0,0 +1,11 @@
+package Testcases;
+
+public class push {
+
+	public static void main(String[] args) {
+	}
+		
+		public static void deno() {
+	}
+
+}
